5e43affeb090106c99ed2bab86eda79466eedf11
Maurizio Carella <maurizio.carella27@gmail.com>
2021-09-12 01:44:26 +0200

    Improved accessibility

--- pages/index.tsx
+++ pages/index.tsx
@@ -82,7 +82,7 @@ const fetchRepositories = async () => {
 }
 
 const Social: React.VFC<{social: typeof socials[number]}> = ({social, ...props}) => (
-	<a rel="noopener noreferrer" href={social.link} className="tooltip p-2" target="_blank" data-tip={social.label}>
+	<a rel="noopener noreferrer" href={social.link} className="tooltip p-2" target="_blank" aria-label={social.label} data-tip={social.label}>
 		{social.icon}
 	</a>
 )
@@ -182,7 +182,7 @@ const Home: React.FC<{works: RepositoryType[], forks: RepositoryType[]}> = ({...
 					<div className="p-2 flex items-center absolute top-0 w-full md:static">
 						<div className="flex-grow"/>
 						<div className="tooltip tooltip-bottom tooltip-left" data-tip="Click to toggle theme">
-							<button type="button" className="btn btn-ghost" onClick={() => toggleTheme()}>
+							<button type="button" className="btn btn-ghost" onClick={() => toggleTheme()} aria-hidden={true} aria-label="Toggle theme">
 								<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 									<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"/>
 								</svg>
