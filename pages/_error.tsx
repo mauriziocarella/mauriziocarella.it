71bd68849cb3697c60ab9bcbc3a9cccaa4036ac4
Maurizio Carella <maurizio.carella27@gmail.com>
2022-08-17 03:54:16 +0200

    Improved Error page

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -1,14 +1,44 @@
 import {NextPageContext} from 'next';
-import React from 'react';
-import {ErrorProps} from 'next/error';
+import React, {useMemo} from 'react';
+import {EmojiSadIcon} from '@heroicons/react/outline';
 
-const Error = ({statusCode}: ErrorProps): JSX.Element => {
-	return <p>{statusCode ? `An error ${statusCode} occurred on server` : 'An error occurred on client'}</p>;
+type ErrorProps = {
+	code: number;
+	message: string;
+};
+const Error = ({code, ...props}: ErrorProps) => {
+	const message = useMemo(() => {
+		if (props.message) return props.message;
+
+		switch (code) {
+			case 404:
+				return 'Page not found';
+		}
+	}, [code, props.message]);
+
+	return (
+		<div className="fixed inset-0 flex items-center justify-center">
+			<div className="text-center max-w-lg space-y-4">
+				<EmojiSadIcon className="w-32 inline-block" />
+
+				<div className="font-medium text-2xl">Something gone wrong</div>
+
+				<div className="font-light tracking-wide">
+					{message} ({code})
+				</div>
+
+				<a className="inline-block text-sm" href="/">
+					mauriziocarella.it
+				</a>
+			</div>
+		</div>
+	);
 };
 
 Error.getInitialProps = ({res, err}: NextPageContext) => {
-	const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
-	return {statusCode};
+	const code = res?.statusCode || err?.statusCode;
+	const message = res?.statusMessage || err?.message;
+	return {code, message};
 };
 
 export default Error;
