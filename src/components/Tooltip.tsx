8fc2569519abe807ec786c4e1b3404b6c32fbe45
Maurizio Carella <maurizio.carella27@gmail.com>
2025-03-17 21:43:52 +0100

    style: minor style changes

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -23,7 +23,13 @@ export const Tooltip = <Element extends ElementType>({
 	return (
 		<Element className={clsx('group relative', className)} {...props}>
 			{children}
-			<div className="absolute z-tooltip bg-stone-800 text-white text-sm py-1 px-2 rounded shadow-lg top-0 left-1/2 opacity-0 -translate-x-1/2 translate-y-0 transition-all group-hocus:opacity-100 group-hocus:-translate-y-full">
+			<div
+				className={clsx(
+					'absolute z-tooltip bg-stone-800 text-white text-sm py-1 px-2 rounded shadow-lg top-0 left-1/2 opacity-0',
+					'-translate-x-1/2 translate-y-0 transition-all',
+					'group-focus-within:opacity-100 group-focus-within:-translate-y-full',
+					'group-hover:opacity-100 group-hover:-translate-y-full',
+				)}>
 				{content}
 			</div>
 		</Element>
