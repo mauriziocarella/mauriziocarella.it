32c2eab4ac0615642a3006f0718aa3fa9998bff1
Maurizio Carella <maurizio.carella27@gmail.com>
2025-05-02 18:49:00 +0200

    feat: add iubenda loading scripts to `Analytics` component

--- src/components/providers/Analytics.tsx
+++ src/components/providers/Analytics.tsx
@@ -1,13 +1,66 @@
-import {GoogleTagManager} from '@next/third-parties/google';
+'use client';
 
-export const Analytics = () => (
-	<>
-		<GoogleTagManager gtmId="GTM-TNKVNFGM" />
-		{/*<Script id="iubenda">*/}
-		{/*	{`var _iub = _iub || [];_iub.csConfiguration = {"ccpaAcknowledgeOnDisplay":true,"consentOnContinuedBrowsing":false,"countryDetection":true,"enableCcpa":true,"floatingPreferencesButtonDisplay":"anchored-bottom-right","floatingPreferencesButtonIcon":false,"invalidateConsentWithoutLog":true,"perPurposeConsent":true,"siteId":2756161,"whitelabel":false,"cookiePolicyId":53338723,"lang":"it","floatingPreferencesButtonCaption":true,"banner":{"acceptButtonDisplay":true,"closeButtonDisplay":false,"customizeButtonDisplay":true,"explicitWithdrawal":true,"listPurposes":true,"position":"float-bottom-center","rejectButtonDisplay":true}};`}*/}
-		{/*</Script>*/}
-		{/*<Script src="//cs.iubenda.com/sync/2756161.js" />*/}
-		{/*<Script src="//cdn.iubenda.com/cs/ccpa/stub.js" />*/}
-		{/*<Script src="//cdn.iubenda.com/cs/iubenda_cs.js" />*/}
-	</>
-);
+import {GoogleTagManager} from '@next/third-parties/google';
+import Script from 'next/script';
+import {useEffect, useState} from 'react';
+
+export const Analytics = () => {
+	const [isClient, setIsClient] = useState(false);
+
+	useEffect(() => {
+		setTimeout(() => setIsClient(true), 100);
+	}, []);
+
+	if (!isClient) return null;
+
+	return (
+		<>
+			<GoogleTagManager gtmId="GTM-TNKVNFGM" />
+			<Script id="iubenda-init" strategy="afterInteractive">
+				{`var _iub = _iub || [];
+            _iub.csConfiguration = {
+			    "askConsentAtCookiePolicyUpdate": true,
+			    "enableFadp": true,
+			    "enableLgpd": true,
+			    "fadpApplies": true,
+			    "floatingPreferencesButtonDisplay": "anchored-center-left",
+			    "perPurposeConsent": true,
+			    "preferenceCookie": {
+			        "expireAfter": 180
+			    },
+			    "siteId": 2756161,
+			    "storage": {
+			        "useSiteId": true
+			    },
+			    "usPreferencesWidgetDisplay": "inline-center",
+			    "whitelabel": false,
+			    "cookiePolicyId": 53338723,
+			    "lang": "it",
+			    "floatingPreferencesButtonCaption": true,
+			    "banner": {
+			        "acceptButtonDisplay": true,
+			        "closeButtonDisplay": false,
+			        "customizeButtonDisplay": true,
+			        "explicitWithdrawal": true,
+			        "listPurposes": true,
+			        "ownerName": "mauriziocarella.it",
+			        "position": "float-bottom-center",
+			        "rejectButtonDisplay": true,
+			        "showPurposesToggles": true,
+			        "showTotalNumberOfProviders": true
+			    }
+			}`}
+			</Script>
+			<Script
+				strategy="afterInteractive"
+				src="//cs.iubenda.com/autoblocking/2756161.js"
+				type="text/javascript"
+			/>
+			<Script
+				strategy="afterInteractive"
+				src="//cdn.iubenda.com/cs/iubenda_cs.js"
+				type="text/javascript"
+			/>
+		</>
+	);
+};
