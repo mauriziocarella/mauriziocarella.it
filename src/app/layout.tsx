3185367b4e2a54da8fc0c34c7ac740e637413893
Maurizio Carella <maurizio.carella27@gmail.com>
2025-06-04 23:11:01 +0200

    style: change default border color in dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import '../styles/globals.scss';
+import '../styles/globals.css';
 import type {Metadata, Viewport} from 'next';
 import {type PropsWithChildren, Suspense} from 'react';
 import AppProviders from '@/components/providers';
